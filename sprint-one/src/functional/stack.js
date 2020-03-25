4f64a7fbfbaee21252b36a165e0853e0de7709d5
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 16:05:46 -0700

    Add logic to Stack function
    1. Implement push method
    	a. Store the value in storage using the length of the keys
    	array and adding 1 to that value
    2. Implement pop method
    	a. Check if the lengh of the keys array is greater than 0
    	b. If so, assign a variable to the value that we intend to
    	remove
    	c. Then delete the value from the storage object
    	d. Return the variable we assigned to the removed value
    3. Implement size method
    	a. Get an array of the keys and return the length of it

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,26 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Store the given value in storage with the one plus the length of the keys array as its key
+    storage[Object.keys(storage).length + 1] = value;
+
   };
 
   someInstance.pop = function() {
+    // If the length of the keys array is greater than 0
+    if (Object.keys(storage).length > 0) {
+      // Assign a variable to the value being removed
+      var valueRemoved = Object.values(storage)[Object.values(storage).length - 1];
+      // Remove the value in storage at key equal to the length of the keys array
+      delete storage[Object.keys(storage).length];
+    }
+    // Return the value removed
+    return valueRemoved;
   };
 
   someInstance.size = function() {
+    // Get an array of the keys and return the length of that array
+    return Object.keys(storage).length;
   };
 
   return someInstance;
