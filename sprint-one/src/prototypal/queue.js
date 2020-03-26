c2afda122240256c468ed7671117b6a253daf5a3
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 23:50:55 -0700

    Initialize setup of Queue class and queueMethods
    1. Used Object.create to create a new instance object using
    queueMethods as its prototype. Now, every time we instantiate
    a new object, their prototypes will be queueMethods, which they
    can then inherit properties from.
    2. Stubbed functions for enqueue, dequeue, and size methods in
    queueMethods

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Create instance object using queueMethods as prototype
+  var someInstance = Object.create(queueMethods);
+
+  // Create storage property in someInstance
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // Add enqueue method to Queue prototype
+  enqueue: function (value) {
+
+  },
+  // Add dequeue method to Queue prototype
+  dequeue: function () {
+
+  },
+  // Add size method to Queue prototype
+  size: function () {
+
+  }
+};
 
 
