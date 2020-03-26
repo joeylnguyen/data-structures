59e84ccfff7847e9042c500331d4f3b501238a5c
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 00:10:33 -0700

    Create lengthKeys and lengthValue properties on someInstance and add logic to size property
    Creating the lengthKeys and lengthValue properties will make the code
    easier to read and less error prone.
    
    For the size method, the lengthKeys property was returned

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,6 +7,10 @@ var Queue = function() {
 
   // Create storage property in someInstance
   someInstance.storage = {};
+  // Create variable equal to the length of storage's keys array
+  someInstance.lengthKeys = Object.keys(someInstance.storage).length;
+  // Create variable equal to the length of storage's values array
+  someInstance.lengthValues = Object.values(someInstance.storage).length;
 
   return someInstance;
 };
@@ -14,15 +18,21 @@ var Queue = function() {
 var queueMethods = {
   // Add enqueue method to Queue prototype
   enqueue: function (value) {
-
+    // Set storage at key equal to number of keys in storage equal to value
   },
   // Add dequeue method to Queue prototype
   dequeue: function () {
-
+    // If number of keys is greater than 0
+    // Set a removedString variable equal to the first value in storage
+    // Run a for loop on the keys array of storage
+    // Set storage at current key equal to the value of storage at the next key
+    // Delete the last property in storage
+    // Return the removedString variable
   },
   // Add size method to Queue prototype
   size: function () {
-
+    // Return the length of storage's keys array
+    return this.lengthKeys;
   }
 };
 
