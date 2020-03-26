862769fc1ab4bf4e454fdf9d9db64c8f8f981d88
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 02:20:01 -0700

    Pass all tests: add logic to all methods
    Object instances will have access to enqueue and dequeue
    methods. User will be able to add values to end of queue
    and remove values from beginning of queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -9,23 +9,34 @@ var Queue = function() {
 // Add enqueue method to Queue's prototype
 Queue.prototype.enqueue = function (value) {
   // Set key at storage equal to the value, where key is equal to the length of storage's keys array
+  this.storage[this.lengthKeys()] = value;
 };
 
 // Add dequeue method to Queue's prototype
-Queue.protoype.dequeue = function () {
+Queue.prototype.dequeue = function () {
   // If there are keys in storage
-  // Set a removedString variable equal to the first value of storage
-  // Loop over the length of storage's keys array
-  // Set storage at current key equal to the value of storage at the next key
-  // Delete the last value in storage
+  if (this.lengthKeys()) {
+    // Set a removedString variable equal to the first value of storage
+    var removedString = this.storage[0];
+    // Loop over the length of storage's keys array
+    for (var i = 0; i < this.lengthKeys(); i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    // Delete the last value in storage
+    delete this.storage[this.lengthKeys() - 1];
+
+    return removedString;
+  }
 };
 
 // Add size method to Queue's prototpye
 Queue.prototype.size = function () {
   // Return lengthKeys
+  return this.lengthKeys();
 };
 
 // Add lengthKeys method to Queue's prototype
 Queue.prototype.lengthKeys = function () {
   // Return the length of storage's keys array
+  return Object.keys(this.storage).length;
 };
