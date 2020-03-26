249880c31b4e27728ca039fbdcf6f4b4ffcf2133
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 03:04:35 -0700

    Structure class and add pseudo code

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // Create storage method
+    this.storage = {};
+
+  }
+
+  // Create enqueue method
+  enqueue (value) {
+    // Set storage at key equal to value, where key is equal to the length of storage's values
+  }
+
+  // Crete dequeue method
+  dequeue () {
+    // If there are keys in storage
+    // Set removedString variable equal to the first value of storage
+    // delete the first property of storage
+    // return removedString
+  }
+  // Create size method
+  size () {
+    // Return the invocation of lengthKeys
+
+  }
+  // Create lengthKeys method
+  lengthKeys () {
+    // Return the length of storage's keys array
   }
 
 }
