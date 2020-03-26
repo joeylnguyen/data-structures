9101c40fdf370119b09051c66dca4fd75a7746fa
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 23:11:35 -0700

    Change _.each to _.extend and add logic to stackMethods
    1. Add logic to push method
    	1a. Assign storage at key equal to the length of storage's
    	key array to the passed value
    2. add logic to pop method
    	2a. Check if storage's keys array has keys
    	2b. If so, assign a variable to the first value in storage's
    	values array
    	2c. Delete the first value in storage
    	2d. Return the removedString variable

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -7,9 +7,8 @@ var Stack = function() {
     // Create property storage
     storage: {}
   };
-
   //  Extend stackMethods properties onto someInstance
-  _.each(someInstance, stackMethods);
+  _.extend(someInstance, stackMethods);
 
   return someInstance;
 };
@@ -18,18 +17,25 @@ var stackMethods = {
   // Create push method
   push: function (value) {
     // Assign storage at key equal to the length of storage's keys array to the value
+    this.storage[Object.keys(this.storage).length] = value;
   },
   // Create pop method
   pop: function () {
     // If there are keys in storage's keys array
-    // Assign a variable removedString equal to the first value in storage's values array
-    // Delete the first value in storage using the first value in storage's keys array as a key
+    if (Object.keys(this.storage).length > 0) {
+      // Assign a variable removedString equal to the first value in storage's values array
+      var removedString = Object.values(this.storage)[0];
+      // Delete the first value in storage using the first value in storage's keys array as a key
+      delete this.storage[Object.keys(this.storage)[0]];
+      // Return the removedString variable
+      return removedString;
+    }
 
-    // Return the removedString variable
   },
   //Create size method
   size: function () {
     // Return the length of storage's keys array
+    return Object.keys(this.storage).length;
   },
 
 };
