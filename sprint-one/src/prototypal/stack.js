cfd1f3dfa7b5180e1b1e6c7e6d74ebcf43f9cb13
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 01:09:45 -0700

    Add pseudo code

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Create instance object setting stackMethods as the prototype
+  var someInstance = Object.create(stackMethods);
+
+  // Create storage property on someInstance
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Create push method
+  push: function (value) {
+    // Set storage of someInstance at the length of storage's keys array equal to the value
+  },
+  // Create pop method
+  pop: function () {
+    // If there are keys in storage's keys array
+    // Set a removedString variable equal to the last value in storage
+    // Delete the last value in storage
+
+    // Return removedString variable
+  },
+  // Create size method
+  size: function () {
+    // Return the invocation of lengthKeys
+  },
+  // Create lengthKeys method
+  lengthKeys: function () {
+    // return the length of storage's keys array
+  }
+};
 
 
