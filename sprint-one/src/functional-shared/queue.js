e9bd52cac462df2962975e7592a90ca99a30b935
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 21:41:11 -0700

    Move storage object into someInstance object and create pseudo code for methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,12 +2,15 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // Create an object that will be instantiated
-  var someInstance = {};
+  var someInstance = {
+    // Make storage a property of someInstance
+    storage: {},
+  };
 
   // Extend the methods properties of queueMethods onto someInstance
   _.extend(someInstance, queueMethods);
 
-  var storage = {};
+
 
   return someInstance;
 };
@@ -15,15 +18,24 @@ var Queue = function() {
 var queueMethods = {
   // Create property for enqueue
   enqueue: function (value) {
-    console.log('hi');
+    // Add the value to storage with key equal to the length of storage's keys array
   },
   // Create property for dequeue
   dequeue: function () {
-    console.log('bye');
+    // If there are keys in storage's keys array
+    // Create a removedString variable that will be assigned to the first value in storage
+
+    // Loop through the storage object
+    // Set the storage object at the current key equal to the value of the storage object at the next key
+
+    // Delete the last value in the storage object
+
+    // Return the removedString variable
   },
   // Create property for size
   size: function () {
-    console.log('why');
+    // Return the length of storage's keys array
+
   }
 };
 
