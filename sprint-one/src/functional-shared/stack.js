d7a3d5c891a1619a61dd2d74e49896ba95319929
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 23:02:16 -0700

    Set up initial structure of Stack class and stackMethods
    1. Implement pseudo code for Stack and stackMethods
    2. Create instance object with storage property
    3. Extend the properties of stackMethods onto the instance object
    4. Return the instance object

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Create someInstance object
+  var someInstance = {
+    // Create property storage
+    storage: {}
+  };
+
+  //  Extend stackMethods properties onto someInstance
+  _.each(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Create push method
+  push: function (value) {
+    // Assign storage at key equal to the length of storage's keys array to the value
+  },
+  // Create pop method
+  pop: function () {
+    // If there are keys in storage's keys array
+    // Assign a variable removedString equal to the first value in storage's values array
+    // Delete the first value in storage using the first value in storage's keys array as a key
+
+    // Return the removedString variable
+  },
+  //Create size method
+  size: function () {
+    // Return the length of storage's keys array
+  },
+
+};
 
 
