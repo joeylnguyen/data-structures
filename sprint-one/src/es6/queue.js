442f09a60b3f8736b74b840cd82471480980e87a
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 03:10:46 -0700

    Add logic to methods

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -10,23 +10,35 @@ class Queue {
   // Create enqueue method
   enqueue (value) {
     // Set storage at key equal to value, where key is equal to the length of storage's values
+    this.storage[this.lengthKeys()] = value;
   }
 
   // Crete dequeue method
   dequeue () {
     // If there are keys in storage
-    // Set removedString variable equal to the first value of storage
-    // delete the first property of storage
-    // return removedString
+    if (this.lengthKeys()) {
+      // Set removedString variable equal to the first value of storage
+      var removedString = this.storage[0];
+      // Loop over length of storage's keys array
+      for (var i = 0; i < this.lengthKeys(); i++) {
+        // Set storage at current key equal to value at storage of next key
+        this.storage[i] = this.storage[i + 1];
+      }
+      // delete the first property of storage
+      delete this.storage[0];
+      // return removedString
+      return removedString;
+    }
   }
   // Create size method
   size () {
     // Return the invocation of lengthKeys
-
+    return this.lengthKeys();
   }
   // Create lengthKeys method
   lengthKeys () {
     // Return the length of storage's keys array
+    return Object.keys(this.storage).length;
   }
 
 }
