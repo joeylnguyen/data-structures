e60a121112673621954aa0cfaaebb6293a46baf9
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-26 03:19:25 -0700

    Add pseudo code

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // Create storage method
+    this.storage = {};
   }
 
+  // Create push method
+  push () {
+    // Set storage at key equal to value, where key is equal to length of storage's keys array
+  }
+
+  // Create pop method
+  pop () {
+    // If storage has keys
+    // Set removedString variable equal to the last value in storage
+    // Delete the last value in storage
+    // Return removedString
+  }
+  // Create size method
+  size () {
+    // Return invocation of lengthKeys
+  }
+  // Create lengthKeys method
+  lengthKeys () {
+    // Return the length of storage's keys array
+
+  }
 }
