af7731c17a40adc85e1bc4679ce9df77421ee91c
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-25 20:57:20 -0700

    Add logic to Queue class and add pseudo code to queueMethods
    1. Create an object that will be instantiated
    2. Extend the properties of that queueMethods onto our instantiation
    object
    3. Create a storage object

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Create an object that will be instantiated
+  var someInstance = {};
+
+  // Extend the methods properties of queueMethods onto someInstance
+  _.extend(someInstance, queueMethods);
+
+  var storage = {};
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // Create property for enqueue
+
+  // Create property for dequeue
+
+  // Create property for size
+};
 
 
